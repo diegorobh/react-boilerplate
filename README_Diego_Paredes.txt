21e713d2a742ea779aa062e42e5a7dab5a3adb25
Diego Paredes <diegorobh@gmail.com>
2019-01-13 19:21:20 -0500

    first commit react-boilerplate

---
+++ README_Diego_Paredes.txt
@@ -0,0 +1,15 @@
+Hello, nice to meet you!
+
+Install the project dependencies:
+
+npm install
+
+For start the project on the node server, please insert:
+
+npm run dev
+
+That will start your server and my dev server.
+
+Please check out the responsive and loader.
+
+Thank you!
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+
+import '../styles/App.css';
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <h1>This is your react boilerplate, you can use react, bootstrap, ES6 and a webpack dev server</h1>
+      </div>
+    )
+  }
+}
+export default App;
